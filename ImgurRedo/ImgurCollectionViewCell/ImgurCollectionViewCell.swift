3200e8ba5930f2765ed52a040db08a220bba3542
Quy33 <87293639+Quy33@users.noreply.github.com>
2021-11-15 17:01:16 +0700

    Finished making the collectionView

---
+++ ImgurRedo/ImgurCollectionViewCell/ImgurCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ImgurCollectionViewCell.swift
+//  ImgurRedo
+//
+//  Created by Mcrew-Tech on 15/11/2021.
+//
+
+import UIKit
+
+class ImgurCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "ImgurCollectionViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- ImgurRedo/ViewController.swift
+++ ImgurRedo/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imgurCollectionView: UICollectionView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        registerCell()
+        imgurCollectionView?.dataSource = self
+        imgurCollectionView?.delegate = self
+        var layout = UICollectionViewFlowLayout()
+        imgurCollectionView?.collectionViewLayout = layout
     }
 
-
+    private func registerCell() {
+        let nib = UINib(nibName: ImgurCollectionViewCell.identifier, bundle: nil)
+        imgurCollectionView?.register(nib, forCellWithReuseIdentifier: ImgurCollectionViewCell.identifier)
+    }
 }
-
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = imgurCollectionView?.dequeueReusableCell(withReuseIdentifier: ImgurCollectionViewCell.identifier, for: indexPath) as! ImgurCollectionViewCell
+        return cell
+    }
+}
+extension ViewController: UICollectionViewDelegate {
+    
+}
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 128, height: 128)
+    }
+}
